f56531940079800d13385932ed911d2d40f35bc9
Daria <dariasiluyanowa@gmail.com>
2023-12-24 20:44:36 +0300

    добавила интерактивности на кнопки

--- src/components/blocks/ContactItem.jsx
+++ src/components/blocks/ContactItem.jsx
@@ -4,7 +4,7 @@ export default function ContactItem({ href, src, alt, id}) {
   return (
     <li className="contacts-list__item" key={id}>
       <a href={href}>
-      <img className="icon-contacts" src={src} alt={alt} />
+        <img className="icon-contacts" src={src} alt={alt} />
       </a>
     </li>
   )

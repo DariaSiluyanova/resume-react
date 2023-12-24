1d054ebe4d4b260291dc18de6eda17ada349a5b3
Daria <dariasiluyanowa@gmail.com>
2023-12-24 21:25:21 +0300

    transition - интерактивность кнопки

--- src/components/blocks/Btn.jsx
+++ src/components/blocks/Btn.jsx
@@ -6,7 +6,7 @@ export default function Btn({ click, active, link = "/", text, id}) {
   return (
     <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
       <Link to={ link }>
-        <li className={`nav__item link ${active}`} key={ id } onClick={ click }>
+        <li className={`nav__item ${active}`} key={ id } onClick={ click }>
             { text }
         </li>
       </Link>

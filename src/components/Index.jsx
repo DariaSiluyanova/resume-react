77c6f845d42f0d80d09ec9d13ffbba8aa18ec114
Daria <prog-1@goodfil.com>
2023-12-25 13:42:26 +0300

    Конвертировала px to rem

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -7,7 +7,7 @@ import { motion } from 'framer-motion';
 
 const promo = [
   <h3 className="promo__title">Привет!</h3>,
-  <p className="promo__text">Я начинающий <span className="promo__pos">frontend developer</span>,&nbsp;рада познакомиться!&nbsp;На&nbsp;этой страничке ты найдешь всю информацию о моих профессиональных навыках и обязательно не забудь посмотреть моё портфолио!</p>,
+  <p className="promo__text">Я&nbsp;начинающий <span className="promo__pos">frontend&nbsp;developer</span>, рада&nbsp;познакомиться! <div className='promoAdd'>На&nbsp;этой страничке ты&nbsp;найдешь&nbsp;всю&nbsp;информацию о&nbsp;моих&nbsp;профессиональных навыках и&nbsp;обязательно не забудь посмотреть моё портфолио!</div></p>,
 ]
 
 export default function Index() {
--- src/components/blocks/PortfolioItem.jsx
+++ src/components/blocks/PortfolioItem.jsx
@@ -3,28 +3,27 @@ import { motion } from 'framer-motion';
 
 export default function PortfolioItem({id, href, src2x, src1x, text}) {
   return (
-    <motion.div   
-      animate={{ y: 10 }}
-      transition={{
-        ease: "linear",
-        duration: 1,
-        y: { duration: 0.5 }
-      }}
-    >
       <li className="portfolio__item" key={id}>
+        <motion.div   
+          animate={{ y: 10 }}
+          transition={{
+          ease: "linear",
+          duration: 1,
+          y: { duration: 0.5 }
+        }}>
           <a href={href}>
           <img
               className="portfolio__img"
-              srcSet={`${{src1x}}+320w,+${{src2x}} 768w`}
+              srcSet={`${{src1x}} 576w,+${{src2x}} 992pw`}
               src={src2x}
               alt={text}
-              sizes="(max-width: 300px) 320px, (max-width: 768px) 768px"
+              sizes="(max-width: 576px) 576px, (max-width: 992px) 992px"
           />
           <p className="portfolio__info">
             {text}
           </p>
           </a>
+        </motion.div>
       </li>
-    </motion.div>
   )
 }
